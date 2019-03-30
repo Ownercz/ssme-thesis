20be114db910382a4a4feadb12f553bdc28f6a51
Radim Lipovčan <radim@lipovcan.cz>
2019-03-30 18:50:10 +0100

    Chi-square test

---
+++ attachments/sql-queries/users_query_chi.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Linux like 'yes' AND Did_you_ever_need_to_restore_your_wallet like 'yes';
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Windows like 'yes' AND Did_you_ever_need_to_restore_your_wallet like 'yes';
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Linux like 'yes' AND Have_you_ever_been_affected_by_malicious_software like 'yes';
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Windows like 'yes' AND Have_you_ever_been_affected_by_malicious_software like 'yes';
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Linux like 'yes' AND Do_you_work_or_study_in_IT_related_field like 'yes';
+SELECT COUNT(*) FROM users WHERE Which_platforms_do_you_use_to_access_Monero_Windows like 'yes' AND Do_you_work_or_study_in_IT_related_field like 'yes';
+
+
